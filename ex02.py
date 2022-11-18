222f21e72486d095ac9dab4477e4758b3f803ca1
Antonio Aguiar <bartolomeu.abnma@gmail.com>
2022-11-18 17:27:53 -0400

    exercises 01-04

---
+++ ex02.py
@@ -0,0 +1,3 @@
+msg = 'Nice to meet you'
+name = str(input("What's your name:"))
+print(msg,name)
---
+++ ex03.py
@@ -0,0 +1,3 @@
+num1 = int(input('Digite um número '))
+num2 = int(input('Digite outro número '))
+print(f'a soma de {num1} e {num2} é {num1+num2}')
