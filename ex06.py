9986b3ff0a4273ee9f47307e67e6e1b6f4e1aa59
Antonio Bartolomeu Aguiar <bartolomeu.abnma@gmail.com>
2022-11-19 13:34:14 -0400

    exercise 06 completed

---
+++ ex06.py
@@ -0,0 +1,10 @@
+from math import sqrt
+
+num = int(input('informe um numero: '))
+double = num*2
+triple = num*3
+root = sqrt(num)
+
+print('dobro: ', double)
+print('triple: ', triple)
+print('square root: ', root)
